5e6956c2d9315cf2fb86f798018f456fac439a30
Aditya raj <88555779+dryruffian@users.noreply.github.com>
2023-12-05 21:43:01 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -4,9 +4,33 @@
   Transaction - an object like { itemName, category, price, timestamp }.
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
+// {
+//   id: 2,
+//   timestamp: 1656259600000,
+//   price: 20,
+//   category: 'Food',
+//   itemName: 'Burger',
+// },
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let values = {};
+  for (let transaction of transactions) {
+      let category = transaction['category'];
+      let price = transaction['price'];
+      if (category in values) {        
+          values[category] += price;
+      } else {
+          values[category] = price;
+      }
+  }
+  let result = [];
+  for (let category in values) {
+      result.push({category: category, totalSpent: values[category]});
+  }
+  return result;
 }
 
+
+  
+// console.log(calculateTotalSpentByCategory(transactions))
 module.exports = calculateTotalSpentByCategory;
