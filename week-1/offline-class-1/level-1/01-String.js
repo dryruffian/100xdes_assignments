7ae2f8a906b075b436344735c402b8ee41f15fc1
Aditya raj <88555779+dryruffian@users.noreply.github.com>
2023-12-06 12:33:52 +0530

    Update 01-String.js

--- week-1/offline-class-1/level-1/01-String.js
+++ week-1/offline-class-1/level-1/01-String.js
@@ -10,67 +10,67 @@ function getLength(str) {
   console.log("Original String:", str);
   console.log("Length:", str.length);
 }
-getLength("Hello World");
+getLength("Aditya Raj");
 
 // indexOf
 function findIndexOf(str, target) {
   console.log("Original String:", str);
   console.log("Index:", str.indexOf(target));
 }
-findIndexOf("Hello World", "World");
+findIndexOf("Aditya Raj", "Raj");
 
 // lastIndexOf
 function findLastIndexOf(str, target) {
   console.log("Original String:", str);
   console.log("Index:", str.lastIndexOf(target));
 }
-findLastIndexOf("Hello World World", "World");
+findLastIndexOf("Aditya Raj Raj", "Raj");
 
 // slice
 function getSlice(str, start, end) {
   console.log("Original String:", str);
   console.log("After slice:", str.slice(start, end));
 }
-getSlice("Hello World", 0, 5);
+getSlice("AdityaRaj", 1, 3);
 
 // substring
 function getSubstring(str, start, end) {
   console.log("Original String:", str);
   console.log("After substring:", str.substring(start, end));
 }
-getSubstring("Hello World", 0, 5);
+getSubstring("AdityaRaj", 1, 3);
 
 // replace
 function replaceString(str, target, replacement) {
   console.log("Original String:", str);
   console.log("After replace:", str.replace(target, replacement));
 }
-replaceString("Hello World", "World", "JavaScript");
+replaceString("Aditya Raj", "Raj", "JavaScript");
 
 // split
 function splitString(str, separator) {
   console.log("Original String:", str);
   console.log("After split:", str.split(separator));
 }
-splitString("Hello World", " ");
+splitString("Aditya Raj", " ");
 
 // trim
 function trimString(str) {
   console.log("Original String:", str);
   console.log("After trim:", str.trim());
 }
-trimString(" Hello World ");
+trimString(" Aditya Raj ");
 
 // toUpperCase
 function toUpper(str) {
   console.log("Original String:", str);
   console.log("After toUpperCase:", str.toUpperCase());
 }
-toUpper("Hello World");
+toUpper("Aditya Raj");
 
 // toLowerCase
 function toLower(str) {
   console.log("Original String:", str);
   console.log("After toLowerCase:", str.toLowerCase());
 }
-toLower("Hello World");
+toLower("Aditya Raj");
